de9b2c70a8dac89ce47edb2b355bc81b14c5342d
yuvrajthakur1 <yuvrajthakurthakur65@gmail.com>
2025-09-15 12:26:21 +0530

    Bakend added

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
+
 export const api = axios.create({
-  baseURL:"http://localhost:4000/api",
+  baseURL:"https://techbuddy-backend-a95s.onrender.com/api",
   withCredentials:true,
 })
 
