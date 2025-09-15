6cd0d35b28abc17b562e6ee1a4fdcbd6760f5c3f
yuvrajthakur1 <yuvrajthakurthakur65@gmail.com>
2025-09-15 11:09:40 +0530

    toast deleted

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,17 +1,16 @@
 import { redirect } from "react-router-dom";
 import api from "../axios/axios";
 import { Toaster } from "react-hot-toast";
-import { notifySuccess,notifyError } from "../toast/toast";
+
 export default async function loginAction({request}) {
      const formData = await request.formData();
      const plainData = Object.fromEntries(formData);
      try {
        const res = await api.post('/auth/login',plainData);
        console.log(res.data.msg)
-       notifySuccess(res.data.msg);
        return redirect('/profile');
      } catch (error) {
-       notifyError(error.response.message);
+      console.log(error);
        return redirect("/signup");
      }
 
