e5d44bdfdced4d728d3100ff239ff87f5ab2bcd3
yuvrajthakur1 <yuvrajthakurthakur65@gmail.com>
2025-10-03 17:26:10 +0530

    commit

--- src/actions/signupAction.js
+++ src/actions/signupAction.js
@@ -1,5 +1,6 @@
 import { redirect } from "react-router-dom";
 import api from "../axios/axios";
+import Swal from "sweetalert2";
 
 export default async function signupAction({request}){
 
@@ -13,7 +14,15 @@ export default async function signupAction({request}){
   try {
      const res = await api.post("/auth/register",plainFormData);
      const data = res.data;
-     alert(data.msg);
+     await Swal.fire({
+              icon: 'success',
+              title: data.msg,
+              text: 'Sign Up SuccessFu;;',
+              timer: 2000, // Alert will close after 2 seconds
+              showConfirmButton: false,
+              background: '#1f2937', // Custom background for dark theme
+              color: '#ffffff'      // Custom text color
+            });
      return redirect('/login');
   } catch (error) {
     alert(error?.response.data.msg);
